f3ea315bfdcb7cc40026778465ab40aaedb3d16a
Polo <polo.diemunsch@gmail.com>
2024-12-05 02:34:55 +0100

    add kotlin template

---
+++ kotlin/src/template.kt
@@ -0,0 +1,35 @@
+private fun readStr() = readln()
+private fun readInt() = readStr().toInt()
+private fun readStrings() = readStr().split(" +".toRegex())
+private fun readInts() = readStrings().map { it.toInt() }
+private fun ints(s: String) = s.split(" +".toRegex()).map { it.toInt() }
+
+private fun part1(ls: List<String>): Int {
+    var ans = 0;
+
+//    for (l in ls)
+
+
+    return ans;
+}
+
+private fun part2(ls: List<String>): Int {
+    var ans = 0;
+
+//    for (l in ls)
+
+
+    return ans;
+}
+
+fun main() {
+    val ls = mutableListOf<String>();
+    var l = readStr();
+    while (l != "") {
+        ls.addLast(l);
+        l = readStr();
+    }
+
+    println(part1(ls));
+//    println(part2(ls));
+}
